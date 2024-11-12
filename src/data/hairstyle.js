824500903cc6843bbede56d17dc5b14bd7ed41f3
adal <adalmendez02@gmail.com>
2024-11-12 13:47:00 -0600

    Estos son mis cambios

--- src/data/hairstyle.js
+++ src/data/hairstyle.js
@@ -1,95 +1,166 @@
 export const hairstyles = [
-    {
-      id: "1",
-      name: "BuzzCut",
-      displayName: "Buzz Cut",
-      description: "Corte de cabello muy corto y uniforme, ideal para un look fresco y fácil de mantener.",
-      image: require("../../assets/buzzcut.jpg"),
-    },
-    {
-      id: "2",
-      name: "ManBun",
-      displayName: "Man Bun",
-      description: "Recogido en un moño en la parte trasera de la cabeza, perfecto para quienes buscan un estilo moderno y versátil.",
-      image: require("../../assets/manbun.jpg"),
-    },
-    {
-      id: "3",
-      name: "Pompadour",
-      displayName: "Pompadour",
-      description: "Estilo con gran volumen en la parte superior, peinado hacia atrás para un look elegante y clásico.",
-      image: require("../../assets/popadour.jpg"),
-    },
-    {
-      id: "4",
-      name: "UnderCut",
-      displayName: "Under Cut",
-      description: "Corte con los lados y la parte trasera rapados y el cabello largo en la parte superior, ideal para un look audaz y moderno.",
-      image: require("../../assets/undercut.png"),
-    },
-    {
-      id: "5",
-      name: "SlickBack",
-      displayName: "Slick Back",
-      description: "Todo el cabello peinado hacia atrás para un estilo pulido y sofisticado.",
-      image: require("../../assets/slicked.jpg"),
-    },
-    {
-      id: "6",
-      name: "Afro",
-      displayName: "Afro",
-      description: "Estilo de gran volumen natural, ideal para quienes desean lucir su textura de cabello rizada con orgullo.",
-      image: require("../../assets/afro.jpeg"),
-    },
-    {
-      id: "7",
-      name: "BowlCut",
-      displayName: "Bowl Cut",
-      description: "Corte en forma de tazón, con un contorno redondeado en la parte superior y lados más cortos.",
-      image: require("../../assets/bobcut.jpg"),
-    },
-    {
-      id: "8",
-      name: "DoubleBun",
-      displayName: "Double Bun",
-      description: "Estilo con dos moños, uno a cada lado de la cabeza, perfecto para un look divertido y juvenil.",
-      image: require("../../assets/doublebun.jpeg"),
-    },
-    {
-      id: "9",
-      name: "LongStraight",
-      displayName: "Long Straight",
-      description: "Cabello largo y liso, peinado hacia atrás para un estilo simple y elegante.",
-      image: require("../../assets/longstraint.jpg"),
-    },
-    {
-      id: "10",
-      name: "ShortCurlyPixie",
-      displayName: "Short Curly Pixie",
-      description: "Estilo corto y rizado, con los lados y la parte trasera recortados y rizos naturales en la parte superior.",
-      image: require("../../assets/shortcurlypixie.jpg"),
-    },
-    {
-      id: "11",
-      name: "ShortTwintails",
-      displayName: "Short Twin Tails",
-      description: "Cabello corto dividido en dos coletas, perfecto para un look juvenil y original.",
-      image: require("../../assets/shorttwin.jpg"),
-    },
-    {
-      id: "12",
-      name: "ShortStraight",
-      displayName: "Short Straight",
-      description: "Cabello corto y liso peinado hacia atrás, ideal para un look elegante y fácil de manejar.",
-      image: require("../../assets/straint.jpeg"),
-    },
-    {
-      id: "13",
-      name: "CurlyBob",
-      displayName: "Curly Bob",
-      description: "Bob con rizos, para un look voluminoso y lleno de personalidad.",
-      image: require("../../assets/curlebob.jpg"),
-    }
-  ];
-  
-  export default hairstyles;
+  {
+    id: "1",
+    name: "BuzzCut",
+    displayName: "Buzz Cut",
+    description: "Corte de cabello muy corto y uniforme, ideal para un look fresco y fácil de mantener.",
+    image: require("../../assets/buzzcut.jpg"),
+  },
+  {
+    id: "2",
+    name: "ManBun",
+    displayName: "Man Bun",
+    description: "Recogido en un moño en la parte trasera de la cabeza, perfecto para quienes buscan un estilo moderno y versátil.",
+    image: require("../../assets/manbun.jpg"),
+  },
+  {
+    id: "3",
+    name: "Pompadour",
+    displayName: "Pompadour",
+    description: "Estilo con gran volumen en la parte superior, peinado hacia atrás para un look elegante y clásico.",
+    image: require("../../assets/popadour.jpg"),
+  },
+  {
+    id: "4",
+    name: "UnderCut",
+    displayName: "Under Cut",
+    description: "Corte con los lados y la parte trasera rapados y el cabello largo en la parte superior, ideal para un look audaz y moderno.",
+    image: require("../../assets/undercut.png"),
+  },
+  {
+    id: "5",
+    name: "SlickBack",
+    displayName: "Slick Back",
+    description: "Todo el cabello peinado hacia atrás para un estilo pulido y sofisticado.",
+    image: require("../../assets/slicked.jpg"),
+  },
+  {
+    id: "6",
+    name: "Afro",
+    displayName: "Afro",
+    description: "Estilo de gran volumen natural, ideal para quienes desean lucir su textura de cabello rizada con orgullo.",
+    image: require("../../assets/afro.jpeg"),
+  },
+  {
+    id: "7",
+    name: "BowlCut",
+    displayName: "Bowl Cut",
+    description: "Corte en forma de tazón, con un contorno redondeado en la parte superior y lados más cortos.",
+    image: require("../../assets/bobcut.jpg"),
+  },
+  {
+    id: "8",
+    name: "DoubleBun",
+    displayName: "Double Bun",
+    description: "Estilo con dos moños, uno a cada lado de la cabeza, perfecto para un look divertido y juvenil.",
+    image: require("../../assets/doublebun.jpeg"),
+  },
+  {
+    id: "9",
+    name: "LongStraight",
+    displayName: "Long Straight",
+    description: "Cabello largo y liso, peinado hacia atrás para un estilo simple y elegante.",
+    image: require("../../assets/longstraint.jpg"),
+  },
+  {
+    id: "10",
+    name: "ShortCurlyPixie",
+    displayName: "Short Curly Pixie",
+    description: "Estilo corto y rizado, con los lados y la parte trasera recortados y rizos naturales en la parte superior.",
+    image: require("../../assets/shortcurlypixie.jpg"),
+  },
+  {
+    id: "11",
+    name: "ShortTwintails",
+    displayName: "Short Twin Tails",
+    description: "Cabello corto dividido en dos coletas, perfecto para un look juvenil y original.",
+    image: require("../../assets/shorttwin.jpg"),
+  },
+  {
+    id: "12",
+    name: "ShortStraight",
+    displayName: "Short Straight",
+    description: "Cabello corto y liso peinado hacia atrás, ideal para un look elegante y fácil de manejar.",
+    image: require("../../assets/straint.jpeg"),
+  },
+  {
+    id: "13",
+    name: "CurlyBob",
+    displayName: "Curly Bob",
+    description: "Bob con rizos, para un look voluminoso y lleno de personalidad.",
+    image: require("../../assets/curlebob.jpg"),
+  },
+  // Mis cambios, adal
+  {
+    id: "14",
+    name: "CurlyShag",
+    display: "Curly Shag",
+    description: "Un estilo rizado y en capas que añade volumen y dimensión.",
+    image: require("../../assets/curlyshag.jpg")
+  },
+  {
+    id: "15",
+    name: "WavyShag",
+    display: "Wavy Shag",
+    description: "Un corte shag ondulado con capas y ondas despeinadas.",
+    image: require("../../assets/wavyshag.jpg")
+  },
+  {
+    id: "16",
+    name: "FauxHawk",
+    display: "Faux Hawk",
+    description: "Un estilo de faux hawk audaz que da un aspecto rebelde.",
+    image: require("../../assets/fauxhawk.jpg")
+  },
+  {
+    id: "17",
+    name: "Spiky",
+    display: "Spiky Hair",
+    description: "Un peinado con puntas para una apariencia atrevida.",
+    image: require("../../assets/spiky.jpg")
+  },
+  {
+    id: "18",
+    name: "CombOver",
+    display: "Comb Over",
+    description: "Un estilo clásico de peinado con raya al lado.",
+    image: require("../../assets/combover.jpg")
+  },
+  {
+    id: "19",
+    name: "HighTightFade",
+    display: "High Tight Fade",
+    description: "Un corte alto con desvanecimiento que proporciona un aspecto limpio y definido.",
+    image: require("../../assets/hightightfade.jpg")
+  },
+  {
+    id: "20",
+    name: "TwoBlockHaircut",
+    display: "Two Block Haircut",
+    description: "Un corte de dos bloques de moda con contraste entre la parte superior y los lados.",
+    image: require("../../assets/twoblockhaircut.jpg")
+  },
+  {
+    id: "21",
+    name: "TexturedFringe",
+    display: "Textured Fringe",
+    description: "Un flequillo texturizado con volumen y definición.",
+    image: require("../../assets/texturedfringe.jpg")
+  },
+  {
+    id: "22",
+    name: "BluntBowlCut",
+    display: "Blunt Bowl Cut",
+    description: "Una versión moderna del clásico corte de tazón con borde recto.",
+    image: require("../../assets/bluntbowlcut.jpg")
+  },
+  {
+    id: "23",
+    name: "LongWavyCurtainBangs",
+    display: "Long Wavy Curtain Bangs",
+    description: "Cabello largo y ondulado con flequillo estilo cortina para un aspecto suave.",
+    image: require("../../assets/longwavycurtainbangs.jpg")
+  }
+];
+
+export default hairstyles;
